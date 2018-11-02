32691caa64b635b3585c990eae424626f4b24c7b
Gus Vroustouris <gvrousto@purdue.edu>
2018-11-02 12:42:54 -0400

    passing directory info correctly

--- public/script.js
+++ public/script.js
@@ -1,3 +1,30 @@
 (function(){
-  promise = fetch(`/items`, { headers });
-}{})
+  var urlParams = new URLSearchParams(window.location.search);
+  let promise;
+  console.log('in client side');
+  promise = fetch(`/directories`);
+  promise
+        .then(res => res.json())
+        .then(data => displayItems(data));
+
+  function displayItems(directoryList) {
+    console.log('in display item');
+    console.log(directoryList);
+    console.log(directoryList.directories);
+    directoryList.directories.forEach((item) => displayItem(item));
+  }
+
+  function displayItem(item){
+    console.log(item.name);
+    const mainDiv = document.getElementById('main-content-container');
+    let imageDiv = document.createElement('div');
+    let h = document.createElement("H1")                // Create a <h1> element
+    let t = document.createTextNode(item.name);
+    h.appendChild(t);
+    imageDiv.classList.add('entire-container');
+    imageDiv.classList.add('shadow');
+    imageDiv.appendChild(h);
+    mainDiv.appendChild(imageDiv);
+  }
+
+}())
--- server.js
+++ server.js
@@ -1,51 +1,56 @@
 const express = require('express');
 const fs = require('fs');
-const path = require('path');
-
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
-class Item{
-  constructor(name, src, id){
+class Image{
+  constructor(name, src){
       this.name = name;
       this.src = src;
-      this.id = id;
     }
 }
 
-class ItemContainer{
-  constructor(){
-    this.items = [];
-    this.activeItem = 0;
+class Directory{
+  constructor(name, images, id){
+      this.name = name;
+      this.images = [];
+      this.id = id;
+    }
+  addImage(Image){
+    this.images.push(Image);
   }
 }
 
-const itemList = new ItemContainer();
-
-app.get('/items', (req, res) => {
-    res.send(itemList.items);
-});
-
-app.get('/test', (req,res) => {
-  console.log('in get');
-  fs.readdir('./public/Content/Directories', function(err, items) {
-    items.forEach(item => displayItem(item));
-  });
-  res.sendFile(`/index.html`, { root: 'public'});
-});
-
-function displayItem(item){
-  console.log(item);
-  const mainDiv = document.getElementById('main-content-container');
-  let imageDiv = document.createElement('div');
-  let h = document.createElement("H1")                // Create a <h1> element
-  let t = document.createTextNode(item);
-  h.appendChild(t);
-  imageDiv.classList.add('entire-container');
-  imageDiv.classList.add('shadow');
-  imageDiv.appendChild(h);
-  mainDiv.appendChild(imageDiv);
+class DirectoryContainer{
+  constructor(){
+    this.directories = [];
+    this.activeItem = 0;
+  }
+  addItem(Item){
+    this.directories.push(Item);
+  }
 }
+
+const directoryList = new DirectoryContainer();
+
+let directories = fs.readdirSync('./public/Content/Directories');
+directories.forEach((directory)=>{
+      let dir = new Directory(directory, '', directoryList.activeItem);
+      directoryList.addItem(dir);
+      directoryList.activeItem++;
+      let images = fs.readdirSync(`./public/Content/Directories/${directory}`);
+      images.forEach((imageName) =>{
+          let img = new Image(imageName, `./Content/Directories/${directory}/${imageName}`);
+          dir.addImage(img);
+      });
+});
+
+app.get('/directories', (req, res) => {
+  res.send(directoryList);
+});
 
 app.listen(3000, () => console.log(`I'm listening on port 3000`));
